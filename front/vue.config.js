486091150772d46bc74432671150026b92a8da49
BabsD221 <aboubacar.diaw17@gmail.com>
2022-05-31 15:26:35 +0200

    update

--- front/vue.config.js
+++ front/vue.config.js
@@ -5,6 +5,6 @@ module.exports ={
   devServer: {
     proxy: 'http://127.0.0.1:5000'
   },
-  publicPath: process.env.NODE_ENV === "production" ? "/FullStackToDoApp/front" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "front/" : "/",
 
 }
